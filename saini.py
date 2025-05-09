abefc2648a96d8e8e27f11d4d11cbcb92867345f
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-05-09 16:56:27 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -258,7 +258,7 @@ async def download_video(url,cmd, name):
 
 
 async def send_doc(bot: Client, m: Message, cc, ka, cc1, prog, count, name):
-    reply = await m.reply_text(f"**★彡 ᵘᵖˡᵒᵃᵈⁱⁿᵍ 彡★ ...⏳**\n\n📚𝐓𝐢𝐭𝐥𝐞 » {name}\n\n✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎🐦")
+    reply = await m.reply_text(f"**★彡 ᵘᵖˡᵒᵃᵈⁱⁿᵍ 彡★ ...⏳**\n\n📚𝐓𝐢𝐭𝐥𝐞 » {name}\n\n✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ 𝓨𝓸𝓾𝓻 𝓯𝓻𝓲𝓮𝓷𝓭\'𝓼 𝓵𝓸𝓿𝓮𝓻")
     time.sleep(1)
     start_time = time.time()
     await bot.send_document(ka, caption=cc1)
